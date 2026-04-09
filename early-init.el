e0d6b9e25aa5c55572bbfcd0a946a787fcc2a56f
Diego <dvignerono@gmail.com>
2026-04-09 14:57:55 +0200

    maximize the app first time it opens

--- early-init.el
+++ early-init.el
@@ -1,5 +1,8 @@
 ;;; `early-init.el' --- Configurations before init.el  -*- lexical-binding: t; -*-
 
+(add-to-list 'initial-frame-alist '(fullscreen . maximized))
+(add-to-list 'default-frame-alist '(fullscreen . maximized))
+
 (when (eq system-type 'darwin)
   (add-to-list 'default-frame-alist '(undecorated-round . t)))
 
